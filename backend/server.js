8151275b0c0265e9e31e0b349df386514c1dc92f
IT24102347 <It24102347@my.sliit.lk>
2026-04-30 15:29:50 +0530

    add modul.ex....

--- backend/server.js
+++ backend/server.js
@@ -37,4 +37,6 @@ mongoose.connect(process.env.MONGO_URI)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server is running on port ${PORT}`);
+
+    module.exports = app;
 });
