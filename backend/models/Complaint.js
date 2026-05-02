4b2f6020131754eb0657e8aab3fbf6155a2e6266
it24101196 <it24101196@my.sliit.lk>
2026-05-02 08:11:57 +0530

    fixed com. .js

--- backend/models/Complaint.js
+++ backend/models/Complaint.js
@@ -6,100 +6,38 @@ const ComplaintSchema = new mongoose.Schema({
         ref: 'Student',
         required: true
     },
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> bd3b878c5dfc98d0144a091bd4021e74eaff1ccb
-=======
->>>>>>> 4df35ae9 (added by room management)
->>>>>>> 4ba963d5d3baed19f0525d42881e3cf2d2c9bcdd
     subject: {
         type: String,
         required: true
     },
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> bd3b878c5dfc98d0144a091bd4021e74eaff1ccb
-=======
->>>>>>> 4df35ae9 (added by room management)
->>>>>>> 4ba963d5d3baed19f0525d42881e3cf2d2c9bcdd
     message: {
         type: String,
         required: true
     },
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> bd3b878c5dfc98d0144a091bd4021e74eaff1ccb
-=======
->>>>>>> 4df35ae9 (added by room management)
->>>>>>> 4ba963d5d3baed19f0525d42881e3cf2d2c9bcdd
     category: {
         type: String,
         enum: ['Room Issue', 'Maintenance', 'Payment Issue', 'Other'],
         default: 'Other'
     },
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> bd3b878c5dfc98d0144a091bd4021e74eaff1ccb
-=======
->>>>>>> 4df35ae9 (added by room management)
->>>>>>> 4ba963d5d3baed19f0525d42881e3cf2d2c9bcdd
     status: {
         type: String,
         enum: ['Pending', 'In Progress', 'Resolved'],
         default: 'Pending'
     },
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> bd3b878c5dfc98d0144a091bd4021e74eaff1ccb
-=======
->>>>>>> 4df35ae9 (added by room management)
->>>>>>> 4ba963d5d3baed19f0525d42881e3cf2d2c9bcdd
     adminReply: {
         type: String,
         default: ''
     }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 4ba963d5d3baed19f0525d42881e3cf2d2c9bcdd
 }, 
 
 
 { timestamps: true });
-<<<<<<< HEAD
-=======
-=======
-}, { timestamps: true });
->>>>>>> bd3b878c5dfc98d0144a091bd4021e74eaff1ccb
-=======
-}, { timestamps: true });
->>>>>>> 4df35ae9 (added by room management)
->>>>>>> 4ba963d5d3baed19f0525d42881e3cf2d2c9bcdd
+
 
 module.exports = mongoose.model('Complaint', ComplaintSchema);
+
