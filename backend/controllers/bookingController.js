7a48c03f25db505e67bd78834f1a1a252b13cf84
it24102938 <it24102938@my.sliit.lk>
2026-05-02 12:54:16 +0530

    Update bookingController.js

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-//Room Booking Module IT24102938
-=======
->>>>>>> 7a4d3308 (added by payment changes)
 const Booking = require('../models/Booking');
 const Room = require('../models/Room');
 
